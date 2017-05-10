d324ba0d620995fa15004c73fb2df8f11d8ce360
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-05-10 23:12:07 +0200

    deleted obsolete code

--- gebu/src/main/java/de/edgesoft/gebu/controller/EventDisplayController.java
+++ gebu/src/main/java/de/edgesoft/gebu/controller/EventDisplayController.java
@@ -1,15 +1,12 @@
 package de.edgesoft.gebu.controller;
 
 import java.io.IOException;
-import java.io.StringReader;
 import java.io.StringWriter;
-import java.text.MessageFormat;
 import java.time.LocalDate;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import de.edgesoft.edgeutils.datetime.DateTimeUtils;
 import de.edgesoft.gebu.Gebu;
 import de.edgesoft.gebu.jaxb.Event;
 import de.edgesoft.gebu.model.AppModel;
@@ -17,7 +14,6 @@ import de.edgesoft.gebu.model.ContentModel;
 import de.edgesoft.gebu.utils.PrefKey;
 import de.edgesoft.gebu.utils.Prefs;
 import de.edgesoft.gebu.utils.Resources;
-import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
 import javafx.fxml.FXML;
@@ -132,110 +128,6 @@ public class EventDisplayController {
             Gebu.logger.catching(e);
 		}
 
-	}
-
-	/**
-	 * Displays events for given date.
-	 *
-	 * @version 6.0.0
-	 * @since 6.0.0
-	 */
-	public void displayEventsOld(final LocalDate theDate) {
-
-		int iInterval = Integer.parseInt(Prefs.get(PrefKey.INTERVAL));
-
-		StringBuilder sbEvents = new StringBuilder();
-
-		if (AppModel.getData().getContent().getEvent().isEmpty()) {
-			sbEvents.append(String.format("<tr><td>%s</td></tr>", "Es wurden noch keine Ereignisse eingegeben."));
-		} else {
-
-			String sTemp = getTableLines(
-					theDate,
-					((ContentModel) AppModel.getData().getContent()).getSortedFilterEvents(theDate, -iInterval, -1),
-					"past");
-
-			if (!sTemp.isEmpty()) {
-				sbEvents.append(sTemp);
-				sbEvents.append("<tr class=\"empty\" />");
-			}
-
-			sTemp = getTableLines(
-					theDate,
-					((ContentModel) AppModel.getData().getContent()).getSortedFilterEvents(theDate, 0, 0),
-					"present");
-
-			if (!sTemp.isEmpty()) {
-				sbEvents.append(sTemp);
-				sbEvents.append("<tr class=\"empty\" />");
-			}
-
-			sTemp = getTableLines(
-					theDate,
-					((ContentModel) AppModel.getData().getContent()).getSortedFilterEvents(theDate, 1, iInterval),
-					"future");
-
-			if (!sTemp.isEmpty()) {
-				sbEvents.append(sTemp);
-			}
-
-			if (sbEvents.length() == 0) {
-				sbEvents.append(String.format("<tr><td>%s</td></tr>",
-						MessageFormat.format("Im Intervall von &pm; {0} Tagen liegen keine Ereignisse.", Integer.parseInt(Prefs.get(PrefKey.INTERVAL)))));
-			}
-		}
-
-		try {
-
-			Map<String, String> mapContent = new HashMap<>();
-			mapContent.put("content", String.format("<table class=\"display\">%s</table>", sbEvents));
-
-			Template tplDisplay = new Template("display", new StringReader(Resources.loadWebView()), new Configuration(Configuration.VERSION_2_3_26));
-
-			try (StringWriter wrtContent = new StringWriter()) {
-				tplDisplay.process(mapContent, wrtContent);
-				dspEvents.getEngine().loadContent(wrtContent.toString());
-			}
-
-		} catch (IOException | TemplateException e) {
-            Gebu.logger.catching(e);
-		}
-
-	}
-
-	/**
-	 * Returns table lines for a time.
-	 *
-	 * @param theDate date (for age computation)
-	 * @param theEvents list of events
-	 * @param theTime kind of time (past, present, future)
-	 *
-	 * @return table lines for time
-	 *
-	 * @version 6.0.0
-	 * @since 6.0.0
-	 */
-	private static String getTableLines(final LocalDate theDate, final List<Event> theEvents, final String theTime) {
-
-		StringBuilder sbReturn = new StringBuilder();
-
-		theEvents.stream()
-				.forEach(event -> {
-					sbReturn.append(String.format("<tr class=\"%s\">", theTime));
-					LocalDate dteEvent = (LocalDate) event.getDate().getValue();
-					sbReturn.append(String.format("<td>%s</td>", DateTimeUtils.formatDate(dteEvent)));
-					sbReturn.append(String.format("<td>(%s)</td>", theDate.getYear() - dteEvent.getYear()));
-					sbReturn.append(String.format("<td>%s</td>", event.getEventtype().getValue()));
-					sbReturn.append(String.format("<td>%s</td>", event.getTitle().getValue()));
-
-					if (Boolean.parseBoolean(Prefs.get(PrefKey.DISPLAY_CATEGORIES))) {
-						sbReturn.append(String.format("<td>%s</td>", event.getCategory().getValue()));
-					}
-
-					sbReturn.append("</tr>");
-				});
-
-		return sbReturn.toString();
 	}
 
 	/**
