cb908c1d9e2a8c973ac677cefe291bdfc609f279
Ekkart Kleinod <ekleinod@edgesoft.de>
2017-05-16 21:58:59 +0200

    javadoc call

---
+++ gebu/src/main/java/de/edgesoft/gebu/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * Main package containing main application class.
+ *
+ * ## Legal stuff
+ *
+ * Copyright 2016-2017 Ekkart Kleinod <ekleinod@edgesoft.de>
+ *
+ * This file is part of "Das Gebu-Programm".
+ *
+ * "Das Gebu-Programm" is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * "Das Gebu-Programm" is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with "Das Gebu-Programm".  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Ekkart Kleinod
+ * @version 6.0.0
+ * @since 6.0.0
+ */
+package de.edgesoft.gebu;
+
+/* EOF */
